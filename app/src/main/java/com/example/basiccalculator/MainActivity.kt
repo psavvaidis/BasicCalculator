5428a2b684844db23c7d72a83170229335679629
psavvaidis <panagiotissav@gmail.com>
2020-07-29 09:38:18 +0300

    Build Basic UI TextBox and Button Grid

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.basiccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button Works", Toast.LENGTH_LONG).show()
     }
 }
